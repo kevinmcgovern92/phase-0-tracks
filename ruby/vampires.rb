f6d821b44885821ddcf1546da1bf9ac7b2be6e55
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-05-25 15:41:39 -0700

    Add vampire catching logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -21,4 +21,38 @@ def interview
     else
       insurance = false
     end
+  current_year = 2016
+  if
+    (age+birth_year == current_year) && (garlic_pref = true || insurance = true)
+    vampire_grade = 1
+  elsif
+    (age+birth_year == current_year) && (garlic_pref = false || insurance = false)
+    vampire_grade = 2
+  elsif
+    (age+birth_year == current_year) && (garlic_pref = false && insurance = false)
+    vampire_grade = 3
+  elsif
+    name = "Drake Cula", "Tu Fang"
+    vampire_grade = 4
+  else
+    vampire_grade = nil
+  end
+  case vampire_grade
+  when
+    vampire_grade = 1
+      puts "Probably not a vampire"
+  when
+    vampire_grade = 2
+      puts "Probably a vampire"
+  when
+    vampire_grade = 3
+      puts "Almost certainly a vampire"
+  when
+    vampire_grade = 4
+      puts "Definitely a vampire"
+  when
+    vampire_grade = nil
+      puts "Results inconclusive"
+end
+
 end

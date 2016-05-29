d057a60ae43c5c89bcb37649f8e94d6f2eb36aa8
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-05-29 12:39:47 -0700

    Add work from Amanda

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,14 +5,19 @@
 def encrypt(password)
   index = 0
   while index < password.length
-    password[index] = password[index].next!
+    if
+      password[index] == "z"
+      password[index] = "a"
+    else
+      password[index] = password[index].next!
+    end
     if
       password[index] == "!"
       password[index] = " "
     end
     index +=1
   end
-puts password
+p password
 end
 
 #Pseudo code for decrypt method
@@ -29,5 +34,30 @@ def decrypt(password)
   password[index] = alphabet[new_number]
   index += 1
   end
- puts password
+ p password
   end
+
+def interface
+#Ask agent if they want to encrypt or decrypt a password
+puts "Would you like to decrypt or encrypt a password?"
+choice = gets.chomp.downcase
+#Ask agent for password
+puts "Type password:"
+input = gets.chomp
+#Run relevant program, print results
+if choice == "decrypt"
+  decrypt(input)
+elsif choice == "encrypt"
+  encrypt(input)
+else puts "You are not a secret agent. Goodbye"
+end
+puts "goodbye"
+end
+
+#interface
+
+#encrypt("abc")
+#encrypt("zed")
+#decrypt("bcd")
+#decrypt("afe")
+#decrypt(encrypt("swordfish"))

dce78b6a3702f28d0e99d0678240ddfd6d55ba00
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-01 14:16:45 -0700

    Finish pseudocode

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,1 +1,22 @@
 # Pseudocode for interior design program
+# Program will be an interface
+# Program will prompt designer to enter details of client
+  # Name
+    #string
+  # Age
+    #integer
+  # Number of children
+    #integer (none should evaluate as 0)
+  # Decor Theme
+    # String (can't limit clients' creativity :D)
+  # Likes open layout design
+    # Boolean
+# Convert user input to appropriate data type
+  # No to_b method
+  # to_i method passed on symbols didn't work in Release 1 irb practice
+# Print hash after data has been input
+# Give user chance to edit a key or not
+# Print hash again
+# Exit
+  # Still no idea how to do that ^
+

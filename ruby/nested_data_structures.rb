3ae32f7634da746bebec25114c359416f597766f
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-02 14:01:41 -0700

    Add nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,3 +1,71 @@
 coloring_book = {
-
+  track_one: {
+    title: "All We Got",
+    artists: [
+      "Chance the Rapper",
+      "Kanye West",
+      "Chicago Children's Choir"
+      ],
+    producers: [
+      "The Social Experiment",
+      "Kanye West"
+      ],
+    length: "3:23"
+    },
+  track_two: {
+    title: "No Problem",
+    artists: [
+      "Chance the Rapper",
+      "Lil Wayne",
+      "2 Chainz",
+    ],
+    producers: [
+      "Brasstracks"
+    ],
+    length: "5:05"
+  },
+  track_three: {
+    title: "Summer Friends",
+    artists: [
+      "Chance the Rapper",
+      "Jeremih",
+      "Francis and the Lights"
+    ],
+    producers: [
+      "Francis and the Lights"
+    ],
+    length: "4:50"
+  },
+  track_four: {
+    title: "D.R.A.M. Sings Special",
+    artists: [
+      "Chance the Rapper"
+    ],
+    producers: [
+      "The Social Experiment",
+      "Jordan Ware"
+    ],
+    length: "1:41"
+  },
+  track_five: {
+    title: "Blessings",
+    artists: [
+      "Chance the Rapper"
+    ],
+    producers: [
+      "The Social Experiment"
+    ],
+    length: "3:41"
+  },
+  track_six: {
+    title: "Same Drugs",
+    artists: [
+      "Chance the Rapper"
+    ],
+    producers: [
+      "The Social Experiment",
+      "Lido"
+    ],
+    length: "4:17"
+  }
 }

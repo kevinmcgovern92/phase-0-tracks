6a835a7fcf15713f3818a13a1903a490a8790ece
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-03 11:31:03 -0700

    Initial commit; add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,13 @@
+#Pseudocode for alias method
+# Prompt user for real name
+  # Assume input to be one string of two words
+  # Downcase
+# Swap the first and last letters of the name
+  # Potentially by assigning to a new array?
+# Change all vowels to the next vowel
+  # Use a string of "aeiou" in the same way that we used the alphabet string for the decrypter
+# Change all consonants to the next consonant
+  # need edge cases for the letters preceding vowels - z, d, h, n, t
+# Use capitalize method before returning new name
+# Return new name
+

9d5b0562735b8715180b849d9bbf7c5bdf3b8a83
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-06 10:26:30 -0700

    Add puppy class comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Puppy Class
+# Species: Puppius Cuteus
+# Characteristics:
+# Ears: Floppy, 2
+# Eyes: 2
+# Legs: 4
+# Color: Golden (varies)
+# Fur: Fluffy (varies)
+# Name: varies
+# Friendly: yes
+# Behavior
+# Wag tail
+# Lick humans
+# Chase tennis balls
+# Mischief

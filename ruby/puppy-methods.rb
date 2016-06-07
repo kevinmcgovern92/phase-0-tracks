b7e6f184d506da8f8455a4dc2d5a5f8bacd0af1a
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-07 10:10:12 -0700

    Add speak method

--- ruby/puppy-methods.rb
+++ ruby/puppy-methods.rb
@@ -2,7 +2,12 @@ class Puppy
   def fetch(toy)
     puts "I brought back the #{toy}"
   end
+
+  def speak(x)
+    x.times{puts "Woof"}
+  end
 end
 
 rex = Puppy.new
 rex.fetch("ball")
+rex.speak(5)

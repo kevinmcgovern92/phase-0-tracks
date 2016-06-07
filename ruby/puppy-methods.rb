3aea7b562143f7f6a8a01645e51ed6436d55106f
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-07 10:11:48 -0700

    Add roll_over and dog_years methods

--- ruby/puppy-methods.rb
+++ ruby/puppy-methods.rb
@@ -6,8 +6,17 @@ class Puppy
   def speak(x)
     x.times{puts "Woof"}
   end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+  def dog_years(x)
+    p x*7
+  end
 end
 
 rex = Puppy.new
 rex.fetch("ball")
 rex.speak(5)
+rex.roll_over
+rex.dog_years(7)

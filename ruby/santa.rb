77907807e99c464a2e236b6e68a2cc31eeaa47b4
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-08 13:12:24 -0700

    Update driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -39,15 +39,22 @@ class Santa
     @gender = new_gender
     p @gender
   end
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
 
 end
 
-#kris = Santa.new("Male", "Caucasian")
+kris = Santa.new("Male", "Caucasian")
 #kris.speak
 #kris.eat_milk_and_cookies("Oatmeal butterscotch cookie")
-santas = []
-genders = ["some sex", "female", "male", "gender fluid", "gender queer", "trans", "none of your business!"]
-ethnicities = ["white", "black", "latino", "native american", "Samoan", "Rasta", "none of your business!"]
+#santas = []
+#genders = ["some sex", "female", "male", "#gender fluid", "gender queer", "trans", "#none of your business!"]
+#ethnicities = ["white", "black", "latino", "#native american", "Samoan", "Rasta", "none #of your business!"]
 # Is there a reason that an array is preferable to a hash here? Maybe because we want to be able to see all the values of the array for demographics?
 if genders.length > ethnicities.length
   genders.length.times do |i|
@@ -58,6 +65,11 @@ else
     santas << Santa.new(genders[i], ethnicities[i])
   end
 end
-santas.each do |santa|
-  santa.sound_off
-end
+#santas.each do |santa|
+#  santa.sound_off
+#end
+kris.get_mad_at("Rudolph")
+kris.celebrate_birthday
+kris.change_gender= "Prince"
+kris.age
+kris.ethnicity

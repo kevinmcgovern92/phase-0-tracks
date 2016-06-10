3b28b8d88d12f913ce38f60260ee83bee3dec3c3
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-10 14:48:04 -0700

    Make Shout a mixin module, add driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,38 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-
-  def self.yell_happily(words)
-    words + "!!" + " :D !!"
-  end
-end
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#
+#  def self.yell_happily(words)
+#    words + "!!" + " :D !!"
+#  end
+#end
 
 #p Shout.yell_happily("I'm quitting my job")
 #p Shout.yell_angrily("I hope all the bad things in the world happen to you and only you")
 
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    words + "!!!!" + " :D"
+  end
+end
+
+class NewYorker
+  include Shout
+end
+
+class SportsFan
+  include Shout
+end
+
+filardi = NewYorker.new
+p filardi.yell_angrily("Everything is so expensive")
+p filardi.yell_happily("I'm moving to Italy in a few weeks")
+
+drake = SportsFan.new
+p drake.yell_angrily("The Raptors lost to the Cavs")
+p drake.yell_happily("The Warriors are gonna beat the Cavs")

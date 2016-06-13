2ebc54ab96cb362b93968b6371beb1ce921ae0f8
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-13 12:26:15 -0700

    Add reverse function and driver code

--- js/explore.js
+++ js/explore.js
@@ -4,5 +4,18 @@
 // Assign each letter to the appropriately reversed index in a new variable
   // Use a FOR loop
   // Condition will likely be i < str.length
-  // Cannot use -1 index
+  // Cannot use -1 as index, so must count up
+    // Can use i = str.length - 1 to mean the same thing
+  // Returning empty variable in Repl - string addition is not working
 // output: string in reverse order
+//
+
+function reverse(string) {
+  emptyString = '';
+  for (var i = string.length - 1; i >= 0; i--) {
+    emptyString += string[i];
+  }
+  console.log(emptyString);
+}
+
+reverse("hello")

f448cb148e4d56a56f0fe5696b33964120cccb1f
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-13 11:55:27 -0700

    Initial commit

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Pseudo code for reverse function
+// input: string
+// Steps: Iterate over the function
+// Assign each letter to the appropriately reversed index in a new variable
+  // Use a FOR loop
+  // Condition will likely be i < str.length
+  // Cannot use -1 index
+// output: string in reverse order

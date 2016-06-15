78e781deef8e15cfe870b71b9ede1e0867484e66
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-15 14:33:22 -0700

    Add pseudocode for longest phrase function

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// Pseudocode for longest phrase function
+// Input: Array of strings
+// Output: Longest string
+// Operations:
+  // Loop through the Array
+    // Likely using a FOR loop based on 'i < array.length'
+  // Measure the length of each value (string) in the array
+  // Print the longest string to the console

ee20b1fc88e8b121e41d8b0828b917340938114d
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-25 11:16:33 -0700

    Add basic interface

--- databases/persistent_data/to_do_app.rb
+++ databases/persistent_data/to_do_app.rb
@@ -14,3 +14,36 @@ create_table_cmd = <<-SQL
     )
 SQL
 
+to_do_data.execute(create_table_cmd)
+
+reveal_all = to_do_data.execute("SELECT * FROM items")
+
+def add_item(db, description, urgency, due_date)
+  db.execute("INSERT INTO items (description, urgency, due_date) VALUES(#{description}, #{urgency}, #{due_date}")
+end
+
+puts "Hello. This is your command line to do list app"
+puts "What is today's date?"
+today = gets.chomp
+today_date = Date.parse(today)
+puts "Here is what you have lined up for today:"
+to_do_data.execute("SELECT * FROM items WHERE due_date = #{today_date}")
+puts "Anything to add to the list?"
+decision = gets.chomp
+if decision.downcase == "yes"
+  puts "Okay. How many items?"
+  operation_number = gets.chomp.to_i
+  operation_number.times do
+    puts "What do you need to do?"
+    descr = gets.chomp
+    puts "How urgent is it?"
+    urgent = gets.chomp
+    puts "When does it need to be done?"
+    due_on = gets.chomp
+    due_date = Date.parse(due_on)
+    add_item(to_do_data, descr, urgent, due_date)
+  end
+else
+  puts "Well if that's all, I think we're done here."
+end
+reveal_all

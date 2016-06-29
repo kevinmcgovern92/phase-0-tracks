7ea348697e17c212d5b38da1d4af1809981b3987
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-29 14:17:31 -0700

    Add work with Brett Buller

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -11,5 +11,11 @@ function changeFont() {
   paragraph[0].style.fontFamily= "Arial"
 };
 
+function changeSpacing() {
+  paragraph[0].style.letterSpacing= "10px"
+};
+
+paragraph[0].addEventListener("click", changeSpacing);
+
 createBorder();
 changeFont();

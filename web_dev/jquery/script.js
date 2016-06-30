afb18b38b16743b55c0c0eba21b4998a7bc70fd1
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-30 16:35:00 -0700

    Add jquery code for click based activity

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,2 +1,11 @@
 console.log("jquery is working");
 
+$(document).ready(function() {
+  $('div').click(function() {
+    $(this).append("<div></div>");
+    $(this).toggleClass('vanish');
+  });
+  $('.vanish').click(function() {
+    $(this).remove();
+  });
+});

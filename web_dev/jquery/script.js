4c1445c53a3246912c27a38aa0c4cec8c190b11b
kevinmcgovern92 <kevin.mcgovern92@gmail.com>
2016-06-30 18:11:20 -0700

    Went far over time box limit, wasn't able to achieve intended functionality.

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,8 +1,9 @@
-console.log("jquery is working");
-
 $(document).ready(function() {
   $('div').click(function() {
     $(this).append("<div></div>");
+    $(this).addClass('create');
+  });
+  $('.create').click(function() {
     $(this).toggleClass('vanish');
   });
   $('.vanish').click(function() {
